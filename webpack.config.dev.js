ed1a70f3bee708a40ce8d7a4b5ffaacef21330b9
Mark_Waldorn <C15501723@mydit.ie>
2018-02-08 15:32:59 +0200

    Fixed dependanceisces

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,21 @@
+import path from 'path'
+
 export default {
-    entry: path.join(__dirname, '/client/index.js'),
-    output: {
-        path: '/'
-    }
+  
+  entry: path.join(__dirname, '/client/index.js'),
+  output: {
+    path: '/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname, 'client'),
+        loaders: [ 'babel' ]
+      }
+    ]
+  },
+  resolve: {
+    extentions: [ '', '.js' ]
+  }
 }
